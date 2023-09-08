524188595f6136902daef7ce20cca3ee386258ba
Gxspxr <sernagas1234@gmail.com>
2023-09-08 20:26:29 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View, Switch } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { Provider } from 'react-redux'
+import { store } from './components/Store/store';
 export default function App() {
+  const { colorScheme, toggleColorScheme} = useColorScheme()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+    <View className="flex h-screen w-screen m-auto justify-center items-center bg-slate-300 dark:bg-black"  >
+      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme}  />
+      <Text className="  bg-sky-500 p-2 rounded-md" >up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
